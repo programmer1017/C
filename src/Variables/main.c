e10c68631820c676daddbffd80cabd75f8a95ea4
Junhyuk Lee <junh071017@gmail.com>
2021-02-14 11:14:51 +0900

    type variable

---
+++ src/Variables/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    a = 10;
+    printf("The value of a is %d. \n", a);
+    printf("\n");
+
+    //////////////////////////////////////////////////////////////// Variable 2.
+
+
+    int b;
+    b = 127;
+    printf("b is %d\n", b);
+    printf("b in octal number: %o\n", b);
+    printf("b in demical number: %d\n", b);
+    printf("b in hexadecimal: %x\n", b);
+    printf("\n");
+
+    return 0;
+}
+
