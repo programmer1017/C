30234128ea64b17f349bdab42b4c9dcdbe41d092
Junhyuk Lee <junh071017@gmail.com>
2021-02-14 10:56:17 +0900

    Hello, World!

---
+++ Hello, World!/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //importing header
+
+int main() { //entry of main function
+    printf("Hello, World!\n"); //\n for new line
+    return 0; //program ended with greate status: 0.
+}
