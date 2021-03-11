420127e6e8883eacf316a93c24b5955cf8d19211
Junhyuk Lee <junh071017@gmail.com>
2021-03-11 22:12:32 +0900

    added some comments due to extension

--- src/Hello, World!/main.c
+++ src/Hello, World!/main.c
@@ -4,3 +4,9 @@ int main() { //entry of main function
     printf("Hello, World!\n"); //\n for new line
     return 0; //program ended with greate status: 0.
 }
+
+// test...
+// ? this is question comment
+// ! this is alert
+// *this is... I don't know
+// ! Nice!
