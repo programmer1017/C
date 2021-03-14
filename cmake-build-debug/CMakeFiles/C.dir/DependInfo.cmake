b76039eced63e4d474fcc2aca0eb706684fd8d72
Junhyuk Lee <junh071017@gmail.com>
2021-03-14 13:50:57 +0900

    CMake

---
+++ cmake-build-debug/CMakeFiles/C.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/GithubRelease/C/src/Hello, World!/main.c" "E:/GithubRelease/C/cmake-build-debug/CMakeFiles/C.dir/src/Hello,_World!/main.c.obj"
+  "E:/GithubRelease/C/src/Variables/main.c" "E:/GithubRelease/C/cmake-build-debug/CMakeFiles/C.dir/src/Variables/main.c.obj"
+  "E:/GithubRelease/C/src/input/C_to_F/input.c" "E:/GithubRelease/C/cmake-build-debug/CMakeFiles/C.dir/src/input/C_to_F/input.c.obj"
+  "E:/GithubRelease/C/src/input/If_statement/if.c" "E:/GithubRelease/C/cmake-build-debug/CMakeFiles/C.dir/src/input/If_statement/if.c.obj"
+  "E:/GithubRelease/C/src/input/If_statement/if_2nd/main.c" "E:/GithubRelease/C/cmake-build-debug/CMakeFiles/C.dir/src/input/If_statement/if_2nd/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "C.exe"
+  "C.exe.manifest"
+  "C.pdb"
+  "CMakeFiles/C.dir/src/Hello,_World!/main.c.obj"
+  "CMakeFiles/C.dir/src/Variables/main.c.obj"
+  "CMakeFiles/C.dir/src/input/C_to_F/input.c.obj"
+  "CMakeFiles/C.dir/src/input/If_statement/if.c.obj"
+  "CMakeFiles/C.dir/src/input/If_statement/if_2nd/main.c.obj"
+  "libC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
